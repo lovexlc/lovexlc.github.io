75f7f9b47c91051ee6415843e1948c63a7f39fd5
snow john <106605081+lovexlc@users.noreply.github.com>
2024-12-20 14:54:07 +0800

    Update type.ts

--- src/shared/type.ts
+++ src/shared/type.ts
@@ -15,4 +15,15 @@ export type PageData = {
 
 export type PageDetail = Omit<PageData, "path" | "id"> & { intro: string; html: string };
 
-export type ShortPageData = Omit<PageData, "content"> & { intro: string };
+// 默认封面类型
+type DefaultCover = {
+  src: 'https://via.placeholder.com/728x400?text=Default+Cover';
+  alt: 'https://via.placeholder.com/728x400?text=Default+Cover';
+};
+
+// 使用条件类型处理 cover 字段
+type ShortPageData = Omit<PageData, "content"> & { intro: string } & (
+  PageData["cover"] extends (undefined | null) 
+    ? { cover: DefaultCover } // 如果 cover 为空，则使用默认封面
+    : { cover: PageData["cover"] } // 如果 cover 不为空，则使用原有的 cover 类型
+);
